237c76e9e3b84cf92677344837aa170a6d6cc3d5
Valkyrja <kilyanet@gmail.com>
2021-02-02 00:27:07 +0300

    Commiting test for the registration form

---
+++ src/test/java/tests/FormTests.java
@@ -0,0 +1,61 @@
+package tests;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$x;
+
+
+public class FormTests {
+
+    @BeforeAll
+    static void setup() {
+        Configuration.startMaximized = true;
+    }
+
+
+    @Test
+    void formFullfillmentCheck() {
+        open("https://demoqa.com/automation-practice-form");
+        $(".main-header").shouldHave(text("Practice Form"));
+
+        $("#firstName").setValue("Asia");
+        $("#lastName").setValue("Beta");
+        $("#userEmail").setValue("a@a.com");
+        $("#genterWrapper").find(byText("Male")).click();
+        $("#userNumber").setValue("1111111111");
+        //Calendar
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").click();
+        $(byText("January")).click();
+        $(".react-datepicker__year-select").click();
+        $(byText("1991")).click();
+        $(".react-datepicker__month").find(byText("25")).click();
+        //$("subjects-auto-complete__value-container.subjects-auto-complete__value-container--is-multi").setValue("Mat").find("Math").click();
+
+        $("#subjectsInput").setValue("Mat");
+        $("#subjectsInput").pressEnter();
+        $("#hobbiesWrapper").find(byText("Reading")).click();
+        $("#uploadPicture").uploadFile(new File("src/test/resources/1.jpg"));
+        $("#currentAddress").setValue("Test street 15");
+        $("#stateCity-wrapper").find("#state").click();
+        //$("#state").click();
+        $("#state").find(byText("Rajasthan")).click();
+        $("#city").click();
+        $("#city").find(byText("Jaipur")).click();
+        $("#submit").click();
+
+
+        $(".modal-content").shouldHave(text("Thanks for submitting the form"), text("Asia Beta"), text("a@a.com"), text("Male"), text("1111111111"), text("25 January,1991"), text("Math"), text("Reading"),
+                text("Test street 15"), text("Rajasthan Jaipur"));
+
+
+    }
+}
